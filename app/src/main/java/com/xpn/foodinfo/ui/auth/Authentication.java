0e1a8d5314d2e595a6773e7e1665ab218c34947b
MartinXPN <mirakyanmartin@gmail.com>
2018-11-26 12:49:18 +0400

    minor changes added terms and conditions

--- app/src/main/java/com/xpn/foodinfo/ui/auth/Authentication.java
+++ app/src/main/java/com/xpn/foodinfo/ui/auth/Authentication.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 
 import com.firebase.ui.auth.AuthUI;
+import com.xpn.foodinfo.R;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +13,6 @@ import java.util.List;
 public class Authentication {
 
     private static final List<AuthUI.IdpConfig> providers = Arrays.asList(
-            new AuthUI.IdpConfig.EmailBuilder().build(),
             new AuthUI.IdpConfig.GoogleBuilder().build(),
             new AuthUI.IdpConfig.FacebookBuilder().build(),
             new AuthUI.IdpConfig.TwitterBuilder().build()
@@ -26,8 +26,8 @@ public class Authentication {
                         .setAvailableProviders(providers)
 //                        .setTosAndPrivacyPolicyUrls("https://foodinfo.github.io/FoodInfoAndroid/privacy_policy",
 //                                                    "https://foodinfo.github.io/FoodInfoAndroid/privacy_policy")
-//                        .setLogo(R.drawable.my_great_logo)      // Set logo drawable
-//                        .setTheme(R.style.MySuperAppTheme)      // Set theme
+                        .setLogo(R.mipmap.ic_launcher)
+                        .setTheme(R.style.AppTheme_AppBarOverlay)
                         .build()
                         .setAction(Intent.ACTION_MAIN)
                         .addCategory(Intent.CATEGORY_HOME)
